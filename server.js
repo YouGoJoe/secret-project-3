01e6ddfe011c3a1a8881a1caf026b000282f2f59
Laith Harb <harblaith77@gmail.com>
2020-10-21 20:56:09 -0400

    set up production and development set of keys

--- server.js
+++ server.js
@@ -10,7 +10,6 @@ if (process.env.NODE_ENV === "production") {
 
 // Send every request to the React app
 // Define any API routes before this runs
-// Comments
 app.get("*", function(req, res) {
   res.sendFile(path.join(__dirname, "./client/build/index.html"));
 });
