510975dabcfab84a203bebb49892944a12f0c4d2
Joe Staller <joestaller23@gmail.com>
2020-10-26 19:08:13 -0400

    extract Nav component from LandingPage

--- client/src/components/LandingPage.jsx
+++ client/src/components/LandingPage.jsx
@@ -1,12 +1,9 @@
 import React from "react";
-import { Navbar, Jumbotron } from "react-bootstrap";
+import { Jumbotron } from "react-bootstrap";
 
 const LandingPage = () => {
   return (
     <div>
-      <Navbar bg="dark" variant="dark">
-        <Navbar.Brand href="/">Drinkr</Navbar.Brand>
-      </Navbar>
       <Jumbotron>
         <h1>Drinkr</h1>
         <p>Consume and compare liquids with friends</p>
