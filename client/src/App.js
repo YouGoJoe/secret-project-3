db2154f06bcefb749fb0570b357d6bfc3e80f6c5
Joe Staller <joestaller23@gmail.com>
2020-10-26 19:08:42 -0400

    adds simple routing to app

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,33 @@
 import React, { Component } from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
 import LandingPage from "./components/LandingPage";
+import Nav from "./components/Nav";
 import "./App.css";
 
 class App extends Component {
   render() {
-    return <LandingPage />;
+    return (
+      <Router>
+        <div>
+          <Nav />
+          <Switch>
+            <Route exact path="/" component={LandingPage} />
+            <Route
+              exact
+              path="/register"
+              component={() => <div>Sign up placeholder</div>}
+            />
+            <Route
+              exact
+              path="/login"
+              component={() => <div>Login placeholder</div>}
+            />
+            <Route component={() => <div>404 Not found</div>} />
+          </Switch>
+        </div>
+      </Router>
+    );
   }
 }
 
