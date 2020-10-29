774fbb67d0545203ddf8df55fc51b698368cb53f
Laith Harb <harblaith77@gmail.com>
2020-10-28 21:02:19 -0400

    created register form that updates component state on input change

--- client/src/pages/RegisterPage.jsx
+++ client/src/pages/RegisterPage.jsx
@@ -1,9 +1,28 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 export default function RegisterPage() {
+
+    const [userInput, setUserInput] = useState({
+        email: "",
+        password: "",
+        confirmPassword: ""
+    })
+
+    const handleChange = (e) => {
+        setUserInput({
+            ...userInput,
+            [e.target.name]: e.target.value
+        })
+    }
+
     return (
         <div>
-            Hello
+            <form>
+                <input name="email" value={userInput.email} onChange={handleChange} type="text" placeholder="email" />
+                <input name="password" value={userInput.password} onChange={handleChange} type="password" placeholder="password" />
+                <input name="confirmPassword" value={userInput.confirmPassword} onChange={handleChange} type="password" placeholder="confirm password" />
+                <input type="submit" value="Sign up" />
+            </form>
         </div>
     )
 }
